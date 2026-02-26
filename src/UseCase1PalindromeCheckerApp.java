5b3af5de183f7794c36275f2f79aea37535c2a81
kamyasinghal <kamyasinghal02@gmail.com>
2026-02-26 14:31:15 +0530

    UC1: Add Welcome message module

---
+++ src/UseCase1PalindromeCheckerApp.java
@@ -0,0 +1,8 @@
+public class UseCase1PalindromeCheckerApp {
+    public static void main(String[] args){
+        System.out.println("Welcome to the Palindrom Checker Management System");
+        System.out.println("Version : 1.0");
+        System.out.println("Author: Kamya");
+        System.out.println("System Initialized Successfully");
+    }
+}
